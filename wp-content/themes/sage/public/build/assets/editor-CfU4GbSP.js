fd296ec57d261d6058420d86c7fdae1cef9c6b21
Yusuf Giovanno <yusufgiovanno91@gmai.com>
2025-10-14 17:57:00 +0700

    Initial commit: WordPress project setup

---
+++ wp-content/themes/sage/public/build/assets/editor-CfU4GbSP.js
@@ -0,0 +1,1 @@
+const d=wp.domReady;d(()=>{});
---
+++ wp-content/themes/sage/resources/views/about/about.blade.php
@@ -0,0 +1,13 @@
+{{--
+  Template Name: Contact Page
+  Template Post Type: page
+--}}
+
+@extends('layouts.app')
+
+@section('content')
+  <div class="container">
+    <h1>{{ get_the_title() }}</h1>
+    <p>This is the contact page template.</p>
+  </div>
+@endsection
